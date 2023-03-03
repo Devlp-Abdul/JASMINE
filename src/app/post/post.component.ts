7dadc0a721b7db0f506699b56f0c6e0ed37e7074
AbdulBasseth <abdulbasee786@gmail.com>
2023-03-03 14:48:41 +0530

    New Commit

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+
+  
+  
+}
